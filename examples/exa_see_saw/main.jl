aafb4e9852171a7fc09cd7d2882ae838909a1f5b
guberger <guillaume.berger@colorado.edu>
2022-07-06 08:57:03 -0600

    Update example see saw

--- examples/exa_see_saw/main.jl
+++ examples/exa_see_saw/main.jl
@@ -7,16 +7,11 @@ using PyPlot
 
 include("../../src/CEGISPolyhedralBarrier.jl")
 CPB = CEGISPolyhedralBarrier
-Halfspace = CPB.Halfspace
-AffForm = CPB.AffForm
-Point = CPB.Point
 Polyhedron = CPB.Polyhedron
 PolyFunc = CPB.PolyFunc
 System = CPB.System
 InitialSet = CPB.InitialSet
 UnsafeSet = CPB.UnsafeSet
-State = CPB.State
-Region = CPB.Region
 
 include("../utils/plotting2D.jl")
 
@@ -24,10 +19,6 @@ const GUROBI_ENV = Gurobi.Env()
 solver() = Model(optimizer_with_attributes(
     () -> Gurobi.Optimizer(GUROBI_ENV), "OutputFlag"=>false
 ))
-
-## Parameters
-nvar = 2
-nloc = 1
 
 box = Polyhedron()
 CPB.add_halfspace!(box, [-1, 0], -20)
@@ -63,20 +54,18 @@ A = Matrix{Bool}(I, 2, 2)
 b = [2, 1]
 CPB.add_piece!(sys, domain ∩ box, 1, A, b, 1)
 
-iset = InitialSet()
-CPB.add_state!(iset, 1, [0, 0])
+iset = InitialSet{1}()
+CPB.add_point!(iset, 1, [0, 0])
 
-uset = UnsafeSet()
+uset = UnsafeSet{1}()
 udom = Polyhedron()
 CPB.add_halfspace!(udom, [-1, 0], 1)
 CPB.add_halfspace!(udom, [0, 1], 0)
-CPB.add_region!(uset, 1, udom ∩ box)
+CPB.add_domain!(uset, 1, udom ∩ box)
 udom = Polyhedron()
 CPB.add_halfspace!(udom, [-1, 0], 10)
 CPB.add_halfspace!(udom, [0, 1], -4)
-CPB.add_region!(uset, 1, udom ∩ box)
-
-## Plotting
+CPB.add_domain!(uset, 1, udom ∩ box)
 
 # Illustration
 fig = figure(0, figsize=(10, 8))
@@ -92,73 +81,35 @@ for ax in ax_
     ax.plot(0, 0, marker="x", ms=10, c="black", mew=2.5)
 end
 
-for state in iset.states
-    plot_point!(ax_[state.loc], state.point, mc="gold")
+for (loc, points) in enumerate(iset.points_list)
+    for point in points
+        plot_point!(ax_[loc], point, mc="gold")
+    end
+    plot_vrep!(ax_[loc], points, fc="yellow", ec="yellow")
 end
 
-for loc = 1:nloc
-    points = [state.point for state in iset.states if state.loc == loc]
-    plot_vrep!(ax_[loc], points, fc="yellow", ec="black")
-end
-
-for region in uset.regions
-    plot_hrep!(
-        ax_[region.loc], region.domain.halfspaces, nothing, fc="red", ec="red"
-    )
+for (loc, domains) in enumerate(uset.domains_list)
+    for domain in domains
+        plot_hrep!(
+            ax_[loc], domain.halfspaces, nothing, fc="red", ec="red"
+        )
+    end
 end
 
 for piece in sys.pieces
     plot_hrep!(
-        ax_[piece.loc1], piece.domain.halfspaces, nothing, fa=0.1
+        ax_[piece.loc1], piece.domain.halfspaces, nothing, fa=0.1, ec="blue"
     )
 end
 
 ## Learner
-lear = CPB.Learner(nvar, nloc, sys, iset, uset, 0, 0)
-CPB.set_tol!(lear, :rad, 1e-4)
-CPB.set_tol!(lear, :bigM, 1e3)
-
-tracerec = CPB.TraceRecorder()
-status, mpf, niter = CPB.learn_lyapunov!(
-    lear, 1000, solver, solver, tracerec=tracerec
-)
+lear = CPB.Learner{2}((2,), sys, iset, uset)
+status, mpf, iter = CPB.learn_lyapunov!(lear, 1000, solver, solver)
 
 display(status)
 
-for loc = 1:nloc
-    plot_level!(ax_[loc], mpf.pfs[loc].afs, [(-21, -21), (21, 21)])
-end
-
-fig = figure(1, figsize=(15, 8))
-ax_ = fig.subplots(
-    nrows=4, ncols=5,
-    gridspec_kw=Dict("wspace"=>0.2, "hspace"=>0.1),
-    subplot_kw=Dict("aspect"=>"equal")
-)
-
-xlims = (-22, 22)
-ylims = (-22, 22)
-
-for ax in ax_
-    ax.set_xlim(xlims...)
-    ax.set_ylim(ylims...)
-    ax.plot(0, 0, marker="x", ms=10, c="black", mew=2.5)
-end
-
-iters = ceil.(Int, range(1, niter, length=20))
-
-for (i, iter) in enumerate(iters)
-    mpf = tracerec.mpf_list[iter]
-    for loc = 1:nloc
-        plot_level!(ax_[i], mpf.pfs[loc].afs, [(-21, -21), (21, 21)])
-    end
-    for evid in tracerec.pos_evids_list[iter]
-        plot_point!(ax_[i], evid.point, mc="red")
-    end
-    for evid in tracerec.lie_evids_list[iter]
-        plot_point!(ax_[i], evid.point1, mc="green")
-        plot_point!(ax_[i], evid.point2, mc="orange")
-    end
+for (loc, pf) in enumerate(mpf.pfs)
+    plot_level!(ax_[loc], pf.afs, [(-21, -21), (21, 21)])
 end
 
 end # module

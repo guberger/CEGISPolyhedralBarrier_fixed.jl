96a86a991ccd0b97b64bc7029048a18db075c0e3
guberger <guillaume.berger@colorado.edu>
2022-07-08 14:37:33 -0600

    Update verifier

--- src/verifier.jl
+++ src/verifier.jl
@@ -1,10 +1,4 @@
-struct PosPredicate
-    N::Int
-    domain::Polyhedron
-    loc::Int
-end
-
-struct LiePredicate
+struct Predicate
     N::Int
     domain::Polyhedron
     loc1::Int
@@ -14,42 +8,32 @@ struct LiePredicate
 end
 
 struct Verifier
-    pos_predics::Vector{PosPredicate}
-    lie_predics::Vector{LiePredicate}
+    predics::Vector{Predicate}
 end
 
-Verifier() = Verifier(PosPredicate[], LiePredicate[])
+Verifier() = Verifier(Predicate[])
 
-function add_predicate!(verif::Verifier, predic::PosPredicate)
-    push!(verif.pos_predics, predic)
+function add_predicate!(verif::Verifier, predic::Predicate)
+    push!(verif.predics, predic)
 end
 
-function add_predicate!(verif::Verifier, predic::LiePredicate)
-    push!(verif.lie_predics, predic)
-end
-
-## Optim problem
-
-abstract type VerifierProblem end
-
-function _add_variables!(model, N, xmax, rmax)
-    x = @variable(model, [1:N], lower_bound=-xmax, upper_bound=xmax)
-    r = @variable(model, upper_bound=rmax)
-    return x, r
-end
-
-function _verify!(prob::VerifierProblem, pfs, xmax, rmax, solver)
+function _verify!(N, domain, A, b, pf1, af2, xmax, solver)
     model = solver()
-    x, r = _add_variables!(model, prob.N, xmax, rmax)
+    x = @variable(model, [1:N], lower_bound=-xmax, upper_bound=xmax)
 
-    _add_constrs_prob!(prob, model, x, r, pfs)
+    for h in domain.halfspaces
+        @constraint(model, dot(h.a, x) + h.β ≤ 0)
+    end
+    for af1 in pf1.afs
+        @constraint(model, _eval(af1, x) ≤ 0)
+    end
 
-    @objective(model, Max, r)
+    @objective(model, Max, _eval(af2, A*x + b))
 
     optimize!(model)
 
     xopt = has_values(model) ? value.(x) : Float64[]
-    ropt = has_values(model) ? value(r) : -Inf
+    ropt = has_values(model) ? objective_value(model) : -Inf
     ps, ts = primal_status(model), termination_status(model)
     flag = ps == FEASIBLE_POINT && ts == OPTIMAL
     @assert flag || (ps == NO_SOLUTION && ts == INFEASIBLE)
@@ -57,71 +41,15 @@ function _verify!(prob::VerifierProblem, pfs, xmax, rmax, solver)
     return xopt, ropt, flag
 end
 
-## Verif Pos
-struct VerifierPos <: VerifierProblem
-    N::Int
-    domain::Polyhedron
-    loc::Int
-end
-
-function _add_constrs_prob!(prob::VerifierPos, model, x, r, pfs)
-    for h in prob.domain.halfspaces
-        @constraint(model, dot(h.a, x) + h.β ≤ 0)
-    end
-    for lf in pfs[prob.loc].afs
-        @constraint(model, 0 ≥ _eval(lf, x) + r)
-    end
-end
-
-function verify_pos(verif::Verifier, mpf::MultiPolyFunc, xmax, rmax, solver)
-    xopt::Vector{Float64} = Float64[]
-    ropt::Float64 = -Inf
-    locopt::Int = 0
-    for predic in verif.pos_predics
-        prob = VerifierPos(predic.N, predic.domain, predic.loc)
-        x, r, flag = _verify!(prob, mpf.pfs, xmax, rmax, solver)
-        if flag && r > ropt
-            xopt = x
-            ropt = r
-            locopt = predic.loc
-        end
-    end
-    return xopt, ropt, locopt
-end
-
-## Verify Lie
-struct VerifierLie <: VerifierProblem
-    N::Int
-    domain::Polyhedron
-    loc1::Int
-    A::Matrix{Float64}
-    b::Vector{Float64}
-    loc2::Int
-    i2::Int
-end
-
-function _add_constrs_prob!(prob::VerifierLie, model, x, r, pfs)
-    for h in prob.domain.halfspaces
-        @constraint(model, dot(h.a, x) + h.β ≤ 0)
-    end
-    for lf1 in pfs[prob.loc1].afs
-        @constraint(model, _eval(lf1, x) ≤ 0)
-    end
-    val2 = _eval(pfs[prob.loc2].afs[prob.i2], prob.A*x + prob.b)
-    @constraint(model, val2 ≥ r)
-end
-
 function verify_lie(verif::Verifier, mpf::MultiPolyFunc, xmax, rmax, solver)
     xopt::Vector{Float64} = Float64[]
     ropt::Float64 = -Inf
     locopt::Int = 0
-    for predic in verif.lie_predics
-        for i2 in eachindex(mpf.pfs[predic.loc2].afs)
-            prob = VerifierLie(
-                predic.N, predic.domain,
-                predic.loc1, predic.A, predic.b, predic.loc2, i2
-            )
-            x, r, flag = _verify!(prob, mpf.pfs, xmax, rmax, solver)
+    for predic in verif.predics
+        N, domain, A, b = predic.N, predic.domain, predic.A, predic.b
+        pf1 = mpf.pfs[predic.loc1]
+        for af2 in mpf.pfs[predic.loc2].afs
+            x, r, flag = _verify!(N, domain, A, b, pf1, af2, xmax, solver)
             if flag && r > ropt
                 xopt = x
                 ropt = r
--- test/test_verifier.jl
+++ test/test_verifier.jl
@@ -11,8 +11,7 @@ CPB = CEGISPolyhedralBarrier
 Polyhedron = CPB.Polyhedron
 PolyFunc = CPB.PolyFunc
 MultiPolyFunc = CPB.MultiPolyFunc
-PosPredicate = CPB.PosPredicate
-LiePredicate = CPB.LiePredicate
+Predicate = CPB.Predicate
 
 solver() = Model(optimizer_with_attributes(
     HiGHS.Optimizer, "output_flag"=>false
@@ -21,98 +20,12 @@ solver() = Model(optimizer_with_attributes(
 ## Parameters
 N = 2
 
-## Pos infeasible
-verif = CPB.Verifier()
-domain = Polyhedron()
-CPB.add_halfspace!(domain, [1, 1], -1)
-CPB.add_halfspace!(domain, [-1, -1], 2)
-CPB.add_predicate!(verif, PosPredicate(N, domain, 1))
-
-mpf = MultiPolyFunc(2)
-afs_ = [([-0.5, 0.5], -0.5), ([1, 0], 0)]
-for af_ in afs_
-    CPB.add_af!(mpf, 1, af_...)
-end
-
-xmax = 1e3
-x, r, loc = CPB.verify_pos(verif, mpf, xmax, 1e4, solver)
-
-@testset "verify pos infeasible" begin
-    @test r == -Inf
-    @test isempty(x)
-    @test loc == 0
-end
-
-## Pos false #1 #2 #3
-verif = CPB.Verifier()
-domain = Polyhedron()
-CPB.add_halfspace!(domain, [1, 1], -1)
-CPB.add_predicate!(verif, PosPredicate(N, domain, 1))
-
-mpf = MultiPolyFunc(2)
-afs_ = [([-0.5, 0.5], -0.5), ([1, 0], 0)]
-for af_ in afs_
-    CPB.add_af!(mpf, 1, af_...)
-end
-
-xmax = 1e3
-x, r, loc = CPB.verify_pos(verif, mpf, xmax, 1e4, solver)
-
-@testset "verify pos false #1" begin
-    @test r ≈ (xmax + 1)/3
-    @test norm(x, Inf) ≈ xmax
-    @test x ∈ domain
-    @test loc == 1
-end
-
-domain = Polyhedron()
-CPB.add_af!(mpf, 2, [4, 0], 1)
-CPB.add_predicate!(verif, PosPredicate(N, domain, 2))
-
-x, r, loc = CPB.verify_pos(verif, mpf, xmax, 1e4, solver)
-
-@testset "verify pos false #2" begin
-    @test r ≈ 4*xmax - 1
-    @test loc == 2
-end
-
-CPB.add_af!(mpf, 2, [4, 0], 4000)
-
-x, r, loc = CPB.verify_pos(verif, mpf, xmax, 1e4, solver)
-
-@testset "verify pos false #3" begin
-    @test r ≈ (xmax + 1)/3
-    @test loc == 1
-end
-
-# Pos true
-verif = CPB.Verifier()
-domain = Polyhedron()
-CPB.add_halfspace!(domain, [-1, -1], 1)
-CPB.add_predicate!(verif, PosPredicate(N, domain, 1))
-
-mpf = MultiPolyFunc(2)
-afs_ = [([-0.5, 0.5], 0.5), ([1, 0], 0)]
-for af_ in afs_
-    CPB.add_af!(mpf, 1, af_...)
-end
-
-xmax = 1e3
-x, r, loc = CPB.verify_pos(verif, mpf, xmax, 1e4, solver)
-
-@testset "verify pos true" begin
-    @test r ≈ -1/2
-    @test x ≈ [1/2, 1/2]
-    @test x ∈ domain
-    @test loc == 1
-end
-
 ## Lie infeasible
 verif = CPB.Verifier()
 domain = Polyhedron()
 A = [0.5 0.0; 1.0 1.0]
 b = [1, 0]
-CPB.add_predicate!(verif, LiePredicate(N, domain, 1, A, b, 1))
+CPB.add_predicate!(verif, Predicate(N, domain, 1, A, b, 1))
 
 mpf = MultiPolyFunc(2)
 afs_ = [([-1.0, 0.0], 1), ([1.0, 0.0], 1)]
@@ -133,7 +46,7 @@ verif = CPB.Verifier()
 domain = Polyhedron()
 A = [0.5 0.0; 1.0 1.0]
 b = [1, 0]
-CPB.add_predicate!(verif, LiePredicate(N, domain, 1, A, b, 1))
+CPB.add_predicate!(verif, Predicate(N, domain, 1, A, b, 1))
 
 mpf = MultiPolyFunc(2)
 afs_ = [([-1.0, 0.0], -1), ([1.0, 0.0], -1)]
@@ -156,7 +69,7 @@ domain = Polyhedron()
 CPB.add_halfspace!(domain, [-1, 0], 0)
 A = [0.0 0.5; 0.5 0.1]
 b = [0, 1]
-CPB.add_predicate!(verif, LiePredicate(N, domain, 1, A, b, 1))
+CPB.add_predicate!(verif, Predicate(N, domain, 1, A, b, 1))
 
 mpf = MultiPolyFunc(2)
 afs_ = [([-1, 0], -1), ([1, 0], -1), ([0, -1], -1), ([0, 1], -1)]
@@ -179,7 +92,7 @@ domain = Polyhedron()
 CPB.add_halfspace!(domain, [-1, 0], 0)
 A = [0.0 0.5; 0.5 0.1]
 b = [0.0, -0.5]
-CPB.add_predicate!(verif, LiePredicate(N, domain, 1, A, b, 1))
+CPB.add_predicate!(verif, Predicate(N, domain, 1, A, b, 1))
 
 mpf = MultiPolyFunc(2)
 afs_ = [([-1, 0], -1), ([1, 0], -1), ([0, -1], -1), ([0, 1], -1)]
@@ -203,7 +116,7 @@ CPB.add_halfspace!(domain, [-1, -1], -1)
 CPB.add_halfspace!(domain, [-1, 1], -1)
 A = [0.5 -0.25; 0.1 0.5]
 b = [0.0, 0.5]
-CPB.add_predicate!(verif, LiePredicate(N, domain, 2, A, b, 1))
+CPB.add_predicate!(verif, Predicate(N, domain, 2, A, b, 1))
 
 mpf = MultiPolyFunc(2)
 afs_ = [([1, 0], -1), ([0, -1], -1), ([0, 1], -1)]
